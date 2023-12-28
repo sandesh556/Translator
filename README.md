2ae55cce760ed1f658701f8b5a22167e45599a40
Sandesh Menath <sandesh.menath@ladybirdweb.com>
2023-12-28 11:59:28 +0530

    Translator

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Translator
+
+## To translate your lang
+ 1. Navigate to lang forlder you will find a lang.py file.
+ 2. In that file make sure you enter your required key values in a json supported format.
+ 3. Example: {
+               "key" : "value",
+             }
+ 4. And then after all your content is inserted into this run the main.py, and you will see your translation in each folder in php format and then use it in your laravel project.
+
+Note: bsn -> is hr which is croatian and ko is just kr which is korean.
+Also, this library doesn't translate english to british english that will have to be done manually
---
+++ main.py
@@ -0,0 +1,47 @@
+from googletrans import Translator
+import os
+
+
+def translate_file_for_all_languages(source_lang, file_name):
+    translator = Translator()
+
+    source_file_path = f"lang/{source_lang}/{file_name}.py"
+
+    if not os.path.exists(source_file_path):
+        print("Source language file not found.")
+        return
+
+    with open(source_file_path, 'r', encoding='utf-8') as source_file:
+        data = eval(source_file.read())  # Assuming it contains a Python dictionary
+
+        for target_lang in os.listdir("lang"):
+            if target_lang == source_lang or not os.path.isdir(f"lang/{target_lang}"):
+                continue
+
+            target_dir_path = f"lang/{target_lang}"
+            print(target_dir_path)
+
+            translated_data = {}
+            for key, value in data.items():
+                translation = translator.translate(value, src=source_lang, dest=target_lang)
+                translated_data[key] = translation.text
+
+            target_file_path = f"{target_dir_path}/{file_name}.php"
+            formatted_string = "<?php\nreturn " + format_as_php_array(translated_data) + ";\n"
+
+            with open(target_file_path, 'w', encoding='utf-8') as target_file:
+                target_file.write(formatted_string)
+
+            print(f"Translation for {target_lang} complete!")
+
+
+def format_as_php_array(data):
+    formatted_array = "array(\n"
+    for key, value in data.items():
+        formatted_array += f"    '{key}' => '{value}',\n"
+    formatted_array += ")"
+    return formatted_array
+
+
+# Usage example
+translate_file_for_all_languages('en', 'lang')  # Translate 'common.py' from English to all languages
